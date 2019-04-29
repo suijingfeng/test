ff5f2c055b31b951732779e76051ab272ec52bbd
suijingfeng <jingfengsui@gmail.com>
2019-04-30 05:47:45 +0800

    add test data

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # test
 Used for store small programs, can not use gist at beijing now. 
+# TODO
+decision a good hash function 
---
+++ R_ShaderText.h
@@ -0,0 +1,9 @@
+#ifndef R_SHADER_TEXT_H_
+#define R_SHADER_TEXT_H_
+
+void SetShaderTextHashTableSizes(const char * const pText );
+char* FindShaderInShaderText( const char * pShaderName );
+
+void printShaderTextHashTable_f(void);
+
+#endif
